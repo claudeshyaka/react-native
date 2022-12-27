6cfe1a45d3efc7d5230c66f4cbb6eb5069e82e30
claudeshyaka <elieclaude23@gmail.com>
2022-12-27 09:54:23 -0500

    Fixed Exposed Token

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://api.yelp.com/v3/businesses",
+    baseURL: "https://api.yelp.com/v3/xxxxxx",
     headers: {
-        Authorization: "Bearer J487hErbVbt1dO9Id5EesaFsHE0s7bbcwm8JkO0K0Wb8DUtaZDRrK5vwTPNNoG8_c41d2Fv2uMKkoDmZMq1dT4lmLOOVCWMrFXc28uj384Nbi_62PCIdp-0wFlYPYnYx"
+        Authorization: ""
     },
 });
